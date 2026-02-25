45c68c40fac17d31a89e009121f152368811481e
Erick San Martin <ErickSanMartin@ibm.com>
2026-02-25 12:21:54 -0300

    Primer commit

---
+++ src/app/gifs/components/side-menu/side-menu-options/side-menu-options.component.ts
@@ -0,0 +1,35 @@
+import { Component } from "@angular/core";
+import { RouterLink, RouterLinkActive } from "@angular/router";
+
+interface MenuOption{
+  icon: string;
+  label: string;
+  route: string;
+  sublabel: string;
+}
+
+@Component({
+  selector:'gifs-side-menu-options',
+  templateUrl:'./side-menu-options.component.html',
+  imports: [RouterLink,RouterLinkActive]
+})
+
+
+export default class SideMenuOptionsComponent{
+
+  menuOptions:MenuOption[] =[
+    {
+      icon: 'fa-solid fa-chart-line',
+      label: 'Trending',
+      route: '/dashboard/trending',
+      sublabel: 'Gifs Populares',
+    },
+    {
+      icon: 'fa-solid fa-magnifying-glass',
+      label: 'Buscador',
+      route: '/dashboard/search',
+      sublabel: 'Buscar gifs',
+    },
+  ];
+
+}
---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+
+  production: true,
+  companyName: 'Gifs',
+  companyName2: 'App',
+  companySlogan: 'Maneja tus gifs',
+  myName: 'Erick',
+  apiKey: 'PLOy9ma5oc900ltmOjkf24JTbC9wD2X8',
+  giphyUrl: 'https://api.giphy.com/v1'
+
+};
